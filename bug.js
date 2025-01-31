828bd590d03a470a9b06277c00649f6f8b121b95
BugHunter-X <askzenai@gmail.com>
2025-01-31 02:22:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,11 @@
+```javascript
+// Some Tailwind CSS classes are not applied correctly.
+
+/* ... other code ... */
+
+<div class="bg-gray-200 p-4 rounded shadow">
+  <p class="text-xl font-bold">This text should be gray, large and bold</p>
+</div>
+
+/* ... other code ... */
+```
